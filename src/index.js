facac444392357915dc5bd19058a2fa13f0fdc85
yunree <contrib@yunree.net>
2020-10-07 15:09:31 +0200

    fix: Extension loading and doComplete()
    Bumped version to 1.0.1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const { sources, workspace } = require('coc.nvim');
+const { sources } = require('coc.nvim');
 const path = require('path');
 
-const activate = async ctx => {
+exports.activate = async ctx => {
 	const emojiData = require(path.resolve(__dirname, 'emoji.json'));
 	const source = {
 		name: 'emoji-shortcodes',
@@ -9,26 +9,14 @@ const activate = async ctx => {
 		doComplete: async function() {
 			return {
 				items: emojiData.map(e => ({
-					word: e.character,
+					word: e.description + ': ',
 					abbr: `${e.character} ${e.description}`,
 					menu: this.menu,
 					filterText: e.description
 				}))
 			};
-		},
-		onCompleteDone: async (item, opt) => {
-			const { nvim } = workspace;
-			let { linenr, col, input, line } = opt;
-			let buf = Buffer.from(line, 'utf8');
-			let pre = buf.slice(0, col-1);
-			let after = buf.slice(col + input.length);
-
-			await nvim.call('coc#util#setline', [linenr, pre + item.word + after]);
-			await nvim.call('cursor', [linenr, Buffer.byteLength(pre + item.word) + 1]);
 		}
 	};
 
 	ctx.subscriptions.push(sources.createSource(source));
 };
-
-exports = { activate };
